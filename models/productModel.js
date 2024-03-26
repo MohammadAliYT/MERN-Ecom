7299505b36d2b95f23c6999411ff2d8262525f74
Mohammad Ali <glow.on63@gmail.com>
2024-03-26 04:14:25 +0100

    Pricing API Implemented

--- models/productModel.js
+++ models/productModel.js
@@ -1,6 +1,21 @@
 const mongoose = require("mongoose");
 const uniqueValidator = require("mongoose-unique-validator");
 
+const PriceRangeSchema = new mongoose.Schema({
+  minQuantity: {
+    type: Number,
+    required: true,
+  },
+  maxQuantity: {
+    type: Number,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+});
+
 const ProductSchema = mongoose.Schema({
   name: {
     type: String,
@@ -8,88 +23,89 @@ const ProductSchema = mongoose.Schema({
     trim: true,
     unique: true,
   },
-  description: {
-    type: String,
-    required: [true, "Please provide a product description"],
-  },
-  price: {
-    type: Number,
-    default: 0,
-    required: [true, "Please provide a product price"],
-    // minLength: [0, "Price cannot be less than 0"],
-    validate: {
-      validator: function (value) {
-        return typeof value === "number" && value > 0;
-      },
-      message: "Price must be greater than zero",
-    },
-  },
-  ratings: {
-    type: Number,
-    default: 0,
-  },
-  images: [
-    {
-      public_id: { type: String, required: true },
-      url: { type: String, required: true },
-    },
-  ],
-  category: {
-    type: String,
-    required: [true, "Please provide a product category"],
-    enum: [
-      "Electronics",
-      "Books",
-      "Clothing",
-      "Home and Kitchen",
-      "Beauty and Personal Care",
-    ],
-  },
-  stock: {
-    type: Number,
-    required: [true, "Please provide a product stock"],
-    default: 1,
-    validate: {
-      validator: function (value) {
-        return typeof value === "number" && value >= 0;
-      },
-      message: "Stock must be greater than zero",
-    },
-  },
-  numberOfReviews: {
-    type: Number,
-    default: 0,
-  },
-  reviews: [
-    {
-      user: {
-        type: mongoose.Schema.ObjectId,
-        ref: "User",
-        required: true,
-      },
-      name: {
-        type: String,
-        required: true,
-      },
-      rating: {
-        type: Number,
-        required: true,
-      },
-      comment: {
-        type: String,
-        required: true,
-      },
-    },
-  ],
-  user: {
-    type: mongoose.Schema.ObjectId,
-    ref: "User",
-    required: true,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
+  // description: {
+  //   type: String,
+  //   required: [true, "Please provide a product description"],
+  // },
+  priceRange: [PriceRangeSchema],
+  // price: {
+  //   type: Number,
+  //   default: 0,
+  //   required: [true, "Please provide a product price"],
+  //   // minLength: [0, "Price cannot be less than 0"],
+  //   validate: {
+  //     validator: function (value) {
+  //       return typeof value === "number" && value > 0;
+  //     },
+  //     message: "Price must be greater than zero",
+  //   },
+  // },
+  // ratings: {
+  //   type: Number,
+  //   default: 0,
+  // },
+  // images: [
+  //   {
+  //     public_id: { type: String, required: true },
+  //     url: { type: String, required: true },
+  //   },
+  // ],
+  // category: {
+  //   type: String,
+  //   required: [true, "Please provide a product category"],
+  //   enum: [
+  //     "Electronics",
+  //     "Books",
+  //     "Clothing",
+  //     "Home and Kitchen",
+  //     "Beauty and Personal Care",
+  //   ],
+  // },
+  // stock: {
+  //   type: Number,
+  //   required: [true, "Please provide a product stock"],
+  //   default: 1,
+  //   validate: {
+  //     validator: function (value) {
+  //       return typeof value === "number" && value >= 0;
+  //     },
+  //     message: "Stock must be greater than zero",
+  //   },
+  // },
+  // numberOfReviews: {
+  //   type: Number,
+  //   default: 0,
+  // },
+  // reviews: [
+  //   {
+  //     user: {
+  //       type: mongoose.Schema.ObjectId,
+  //       ref: "User",
+  //       required: true,
+  //     },
+  //     name: {
+  //       type: String,
+  //       required: true,
+  //     },
+  //     rating: {
+  //       type: Number,
+  //       required: true,
+  //     },
+  //     comment: {
+  //       type: String,
+  //       required: true,
+  //     },
+  //   },
+  // ],
+  // user: {
+  //   type: mongoose.Schema.ObjectId,
+  //   ref: "User",
+  //   required: true,
+  // },
+  // createdAt: {
+  //   type: Date,
+  //   default: Date.now,
+  // },
 });
 
 ProductSchema.plugin(uniqueValidator, {
--- utils/errorHandler.js
+++ utils/errorHandler.js
@@ -1,8 +1,14 @@
 class ErrorHandler extends Error {
+  //The constructor method is called when you create a new instance of
+  //the ErrorHandler class. It takes two parameters: message and statusCode.
   constructor(message, statusCode) {
+    //super(message) calls the constructor of the parent class (Error) with the provided message.
+    //This sets the error message of the ErrorHandler instance.
     super(message);
     this.statusCode = statusCode;
     Error.captureStackTrace(this, this.constructor);
+    //Error.captureStackTrace(this, this.constructor) captures a stack trace for the error.
+    //This helps in debugging by providing information about where the error occurred in the code.
   }
 }
 module.exports = ErrorHandler;
